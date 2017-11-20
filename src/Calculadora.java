eab35dff70c4f38736dc490ece084ef20d121282
Diego-Alex <aledeac777@gmail.com>
2017-11-20 22:18:52 +0100

    Calculadora Arreglada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -16,29 +16,38 @@ public class Calculadora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado=new Scanner(System.in);
-		String numeros="1234567890";
-		int opc=1;
-		String num1 = "",num2 = "";
-		float result=0;
-		String comprobante="";
-		System.out.println("Introduce dos numeros por favor:");
-		num1 = teclado.next();
-		while (numeros.indexOf(num1)!=1) {
-			System.out.println("Por favor introduzca un numero");
-			num1 = teclado.next();
+		int opc=1; //Opcion que das a elegir al usuario, para que eliga si quiere sumar, restar,...
+		int num1 =0,num2 = 0; //Los numeros con los valores que se realizaran los calculos
+		float result=0; //Resultado de las operaciones
+		String numeros="1234567890"; //Esta variable se utiliza para verificar si se ha introducido un numero correcto
+		String verificador=""; //Variable parecida al comodin, que toma el valor del numero introducido y lo conprueba si esta correcto o no
+		
+		System.out.println("Introduce un numero por favor:");
+		verificador = teclado.next();
+		int i = 0;
+		while ( i < verificador.length()) {
+			if (numeros.indexOf(verificador.substring(i,i+1))==-1) {
+				System.out.println("Por favor introduzca un numero valido");
+				verificador = teclado.next();
+				i=0;
+			}else{
+				i++;
+			}
 		}
-		System.out.println("Introduzca el segundo numero");
-		num2 = teclado.next();
-		
-		while (numeros.indexOf(num2)!=1) {
-			System.out.println("Por favor introduzca un numero");
-			num2 = teclado.next();
+		num1=Integer.parseInt(verificador);
+		System.out.println("introduzca el segundo numero");
+		verificador = teclado.next();
+		i=0;
+		while ( i < verificador.length()) {
+			if (numeros.indexOf(verificador.substring(i,i+1))==-1) {
+				System.out.println("Por favor introduzca un numero valido");
+				verificador = teclado.next();
+				i=0;
+			}else{
+				i++;
+			}
 		}
-		int comodin1=Integer.getInteger(num1);
-		int comodin2=Integer.getInteger(num2);
-		System.out.println("Selecciona una opción por favor:");
-		
-		
+		num2=Integer.parseInt(verificador);
 		System.out.println("OPCIONES:");
 		System.out.println("Opción 1 = Suma");
 		System.out.println("Opción 2 = Resta");
@@ -47,23 +56,22 @@ public class Calculadora {
 		opc = teclado.nextInt();
 		while (!(opc==1||opc==2||opc==3||opc==4)) {
 			System.out.println("Por favor introduzca una opcion valida");
+			opc = teclado.nextInt();
 		}
 		if (opc == 1) {
-			result= comodin1 + comodin2;
-		
-		System.out.println(result);
-			
+			result= num1 + num2;
+		System.out.println(result);	
 		}
 		if (opc == 2) {
-			result = comodin1 - comodin2;
+			result = num1 - num2;
 			System.out.println(result);
 		}
 		if (opc == 3) {
-			result=comodin1*comodin2;
+			result=num1*num2;
 			System.out.println(result);
 		}
 		if (opc == 4) {
-			result=comodin1/comodin2;
+			result=num1/num2;
 			System.out.println(result);	
 		}
 	}
